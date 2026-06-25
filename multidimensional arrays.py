b9b31f806a1967c4b7090e64e6e723559b1d6860
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-06-25 07:35:31 +0200

    arrays

--- multidimensional arrays.py
+++ multidimensional arrays.py
@@ -1,1 +1,8 @@
 import numpy as np 
+
+# this is 0 dimensional array 
+# 0 dimensional array means we pass just any value in our array not a list or a tuple ...etc
+array = np.array('A')
+print(array.ndim) # we use ndim method to know the number of dimensional of an array
+array = np.array()
+print(array.ndim)
