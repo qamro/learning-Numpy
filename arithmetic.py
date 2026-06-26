0d02c7b37f6f31afda40db4db66e860966da983e
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-06-26 03:17:12 +0200

    arithmetic operations

--- arithmetic.py
+++ arithmetic.py
@@ -4,3 +4,9 @@ import numpy as np
 array = np.array([1, 2, 3])
 print(array + 1)
 print(array - 2)
+print(array * 3)
+print(array / 4)
+print(array ** 5)  # ** that means power 
+
+# vectorized math functions
+array = np.array([1, 2, 3])
