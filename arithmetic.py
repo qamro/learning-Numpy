000e95db2c5e10f497218cecb8e030ef7b7f0c42
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-06-26 03:20:53 +0200

    arithmetic operations

--- arithmetic.py
+++ arithmetic.py
@@ -10,3 +10,7 @@ print(array ** 5)  # ** that means power
 
 # vectorized math functions
 array = np.array([1, 2, 3])
+print(np.sqrt(array))
+array2 = np.array([1.09, 2.34, 3.87])
+print(np.round(array2))
+print()
