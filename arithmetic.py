12889d7d237005283cfa66a429bb14e2ee31af29
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-06-26 03:46:47 +0200

    arithmetic operations

--- arithmetic.py
+++ arithmetic.py
@@ -1,5 +1,5 @@
 import numpy as np 
-
+# git commit -m "arithmetic operations"
 # scalar arithmetic
 array = np.array([1, 2, 3])
 print(array + 1)
@@ -7,10 +7,25 @@ print(array - 2)
 print(array * 3)
 print(array / 4)
 print(array ** 5)  # ** that means power 
+print()
 
 # vectorized math functions
 array = np.array([1, 2, 3])
 print(np.sqrt(array))
+print(array * np.pi)
+print(array * np.e)
+print((array ** 2) * np.pi)  # calculating area of circle with radius values in the array
 array2 = np.array([1.09, 2.34, 3.87])
 print(np.round(array2))
+print(np.floor(array2))
+print(np.ceil(array2))
 print()
+
+# arrays arithmetic
+array3 = np.array([1, 2, 3])
+array4 = np.array([4, 5, 6])
+print(array3 + array4)
+print(array3 - array4)
+print(array3 * array4)
+print(array3 / array4)
+print(array3 ** array4)
